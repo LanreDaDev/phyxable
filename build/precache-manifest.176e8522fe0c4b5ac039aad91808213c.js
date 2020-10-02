bcd3f63d804849cb7de566a5c3e25dfe8b62ef9b
Lanre Adebayo <lanre@olade.com>
2020-10-02 13:12:40 -0600

    added the build file"

---
+++ build/precache-manifest.176e8522fe0c4b5ac039aad91808213c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3383356b9deec023d6653d2e13621eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb61d0a9ba168ac18729",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "0bc946e42eac76c78b19",
+    "url": "/static/css/main.fc167971.chunk.css"
+  },
+  {
+    "revision": "bb61d0a9ba168ac18729",
+    "url": "/static/js/2.e4d68c66.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e4d68c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc946e42eac76c78b19",
+    "url": "/static/js/main.1ca1e6f2.chunk.js"
+  },
+  {
+    "revision": "8149231e186890cb60cb",
+    "url": "/static/js/runtime-main.4cc09075.js"
+  }
+]);
